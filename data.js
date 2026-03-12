f515bbaba853e59e287d9212c2089bcfa339c43a
Kritagya Yadav <kritagyay2006@gmail.com>
2026-03-12 21:31:37 +0530

    intial commit

---
+++ data.js
@@ -0,0 +1,39 @@
+const teams = [
+
+{
+TeamName: "Temp Pointers",
+Leader: "Kritagya",
+Member2: "Harshjeet",
+Member3: "Shivanghi",
+Member4: "Krishna",
+Phone: "9876543210",
+UTR: "23456789",
+Payment: "Verified",
+Room: "A204"
+},
+
+{
+TeamName: "Code Ninjas",
+Leader: "Rahul",
+Member2: "Aman",
+Member3: "Priya",
+Member4: "Sneha",
+Phone: "9871112222",
+UTR: "67891234",
+Payment: "Pending",
+Room: "B101"
+},
+
+{
+TeamName: "Byte Builders",
+Leader: "Ankit",
+Member2: "Riya",
+Member3: "Vikas",
+Member4: "Sahil",
+Phone: "9812345678",
+UTR: "99887766",
+Payment: "Verified",
+Room: "C305"
+}
+
+]
